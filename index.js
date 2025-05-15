8eabd2d2dae860fd5eb8b7a0774dc512608872c0
ibra11m <66222453+ibra11m@users.noreply.github.com>
2025-05-15 22:08:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get(targetUrl, {
       headers: {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
       },
     });
     res.send(response.data);
